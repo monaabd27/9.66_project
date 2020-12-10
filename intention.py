ab117ac5f5b9f949a23d115e40b70570dd300a97
monaabd27 <monaabd@mit.edu>
2020-12-10 14:41:37 -0600

    graph stuff

--- intention.py
+++ intention.py
@@ -1,5 +1,6 @@
 import pyAgrum as gum
 import pyAgrum.lib.notebook as gnb
+from dijkstra import DijkstraSPF, Graph
 #import cairosvg
 
 
@@ -36,39 +37,14 @@ def inf_diagram1():
     throw.changeLabel(0,'Yes')
     throw.changeLabel(1,'No')
     model.addDecisionNode(throw)
-    # Add a decision node for drill
-    # drill = gum.LabelizedVariable('Drill','Drill for oil',2)
-    # drill.changeLabel(0,'Yes')
-    # drill.changeLabel(1,'No')
-    # model.addDecisionNode(drill)
-    # # Add a chance node for result of test
-    # result = gum.LabelizedVariable('Result','Result of test',4)
-    # result.changeLabel(0,'NoS')
-    # result.changeLabel(1,'OpS')
-    # result.changeLabel(2,'ClS')
-    # result.changeLabel(3,'NoR')
-    # model.addChanceNode(result)
-    # # Add a chance node for oil amount
-    # amount = gum.LabelizedVariable('Amount','Oil amount',3)
-    # amount.changeLabel(0,'Dry')
-    # amount.changeLabel(1,'Wet')
-    # amount.changeLabel(2,'Soak')
-    # model.addChanceNode(amount)
-    # Add an utility node for testing
     ut1to5 = gum.LabelizedVariable('Util1to5','Utility of 1 to 5',1)
     model.addUtilityNode(ut1to5)
-    # Add an utility node for drilling
+    # Add an utility node 
     ut6 = gum.LabelizedVariable('Util6','Utility of 6',1)
     model.addUtilityNode(ut6)
     # Add connections between nodes
     model.addArc(model.idFromName('Throw A'), model.idFromName('Util1to5'))
     model.addArc(model.idFromName('Throw A'), model.idFromName('Util6'))
-    # model.addArc(model.idFromName('Test'), model.idFromName('UtilityOfTest'))
-    # model.addArc(model.idFromName('Test'), model.idFromName('Drill'))
-    # model.addArc(model.idFromName('Amount'), model.idFromName('Result'))
-    # model.addArc(model.idFromName('Amount'), model.idFromName('UtilityOfDrill'))
-    # model.addArc(model.idFromName('Result'), model.idFromName('Drill'))
-    # model.addArc(model.idFromName('Drill'), model.idFromName('UtilityOfDrill'))
 
     # Add utilities
     model.utility(model.idFromName('Util1to5'))[{'Throw A':'Yes'}]=5
@@ -84,4 +60,61 @@ def inf_diagram1():
     print('Maximum Expected Utility (MEU) : {0}'.format(ie.getMEU()))
 
 
-inf_diagram1()
+
+def inf_diagram2(utils):
+    """
+    method using a graph
+
+    args: max_utils, list in the form [[u1, u2, ..], [u3, u4, ...], [u5, u6, ..], ...]
+    where every sublist represents a course of action that can be taken
+
+    returns: the sublist with the maximum utilty
+
+    this is definition 2 of intention in the paper
+    """
+
+    def create_graph(start, end1, end2, choice):
+        # create the graph so that the throw node connects to one util node
+        #but the utils are added for each group, and then negated
+        #each negated sum will serve as a directed edge to our end node
+        negated_sums = [sum(u)*-1 for u in utils]
+        graph = Graph()
+        graph.add_edge(start, end2, -1)
+        graph.add_edge(start, end1, -5)
+        graph.add_edge(end2, choice, 0)
+        graph.add_edge(end1, choice, 0)
+
+        
+        #add an edge to the graph, where the first arg is start node
+        #second arg is end node, thirs arg is in form (weight, name)
+        #name is the ind of the weight in negated_sums
+        return graph
+
+    def cost_func(u, v, edge, prev_edge):
+        length, name = edge
+        if prev_edge:
+            prev_name = prev_edge[1]
+        else:
+            prev_name = None
+        cost = length
+        # if name != prev_name:
+        #     cost += 0
+        return cost
+
+    start = "throw_A"
+    end1 = "util1to5"
+    end2 = "util6"
+    choice = "end result"
+    nodes = [start, end1, end2, choice]
+    graph = create_graph(start, end1, end2, choice)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(end1))
+    print(dijkstra.get_path(end1))
+    print(dijkstra.get_distance(end2))
+    print(dijkstra.get_path(end2))
+    print(dijkstra.get_distance(choice))
+    print(dijkstra.get_path(choice))
+
+
+inf_diagram2([[1,1,1,1,1],[1]])
+# inf_diagram1()
