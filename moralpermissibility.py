82edd030c11a9ff0b2d8609a7176772f09879895
gmrg <58602940+gmrg@users.noreply.github.com>
2020-12-10 12:32:40 -0400

    Add files via upload

---
+++ moralpermissibility.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 11:52:12 2020
+
+@author: gredondo
+"""
+
+import math
+
+def logit(x, alpha_1, alpha_2):
+    insexp = alpha_1 * (x-alpha_2)
+    logit_def = (1+math.exp(insexp))**(-1)
+    return logit_def
+    
+#p_harm = P(I_harm = Yes|A) = probability that the agent is inferred to have
+#intended to hard someone given that they took action A
+def intention_permissibility(p_harm, alpha_1 =7, alpha_2=0.7):
+    return logit((1-p_harm), alpha_1, alpha_2)
+
+def utility_permissibility(lives_lost, lives_saved, alpha_1 =0.3, alpha_2=0):
+    delta_lives = lives_lost - lives_saved
+    return logit(delta_lives, alpha_1, alpha_2)
+
+def full_permissibility(w, p_harm, lives_lost, lives_saved):
+    per_intention = intention_permissibility(p_harm)
+    per_utility = utility_permissibility(lives_lost, lives_saved)
+    per_full = w*per_intention + (1-w)*per_utility
+    return per_full
+    
+    
+print(full_permissibility(0.5, 0.5, 5, 8))
