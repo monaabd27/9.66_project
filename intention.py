40503ede9026974e5450e115b73e6b6970dbd2ff
monaabd27 <monaabd@mit.edu>
2020-12-10 21:29:27 -0600

    tracks tested in paper

--- intention.py
+++ intention.py
@@ -12,7 +12,22 @@ def life_value(norm, alpha_norm, n, k):
         D_T = alpha_norm*k*np.random.exponential()
     return D_T
 
-print(life_value(True, 0.5, 0.5, 0.5))
+#the way the norms impact is thru the utility
+#for a given desire and norm we have our utility, 
+# we can find the utility and best descision
+#we dont know the desires and norms from the influence diagram
+#the general influence diagram is known
+#find the probability of intention given an influence diagram
+#given an intention you know what actions that person should take
+#find the posterior through rejection sampling
+#this part just gives us the desires
+#not directly related to the intention
+
+#moral permissibility section is fitting the model to the human data
+#we jsut use their parameters
+#run mini experiments on ppl and then fit the model
+
+#print(life_value(True, 0.5, 0.5, 0.5))
 
 # from scipy.stats import rv_discrete
 # class life_value(rv_discrete):
@@ -83,67 +98,188 @@ def inf_diagram1():
 
 
 
-def inf_diagram2(utils):
-    """
-    method using a graph
-
-    args: max_utils, list in the form [[u1, u2, ..], [u3, u4, ...], [u5, u6, ..], ...]
-    where every sublist represents a course of action that can be taken
-
-    returns: the sublist with the maximum utilty
-
-    this is definition 2 of intention in the paper
-    """
-
-    def create_graph(start, end1, end2, choice):
-        # create the graph so that the throw node connects to one util node
-        #but the utils are added for each group, and then negated
-        #each negated sum will serve as a directed edge to our end node
-        negated_sums = [sum(u)*-1 for u in utils]
-        graph = Graph()
-        graph.add_edge(start, end2, -1)
-        graph.add_edge(start, end1, -5)
-        graph.add_edge(end2, choice, 0)
-        graph.add_edge(end1, choice, 0)
-
-        
-        #add an edge to the graph, where the first arg is start node
-        #second arg is end node, thirs arg is in form (weight, name)
-        #name is the ind of the weight in negated_sums
-        return graph
-
-    def cost_func(u, v, edge, prev_edge):
-        length, name = edge
-        if prev_edge:
-            prev_name = prev_edge[1]
-        else:
-            prev_name = None
-        cost = length
-        # if name != prev_name:
-        #     cost += 0
-        return cost
-
+def inf_diagram5v1():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem
+    graph = Graph()
     start = "throw_A"
     end1 = "util1to5"
     end2 = "util6"
     choice = "end result"
-    nodes = [start, end1, end2, choice]
-    graph = create_graph(start, end1, end2, choice)
+    graph.add_edge(start, end1, -5)
+    graph.add_edge(start, end2, -1)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
     dijkstra = DijkstraSPF(graph, start)
-    print(dijkstra.get_distance(end1))
-    print(dijkstra.get_path(end1))
-    print(dijkstra.get_distance(end2))
-    print(dijkstra.get_path(end2))
-    print(dijkstra.get_distance(choice))
+    print(dijkstra.get_distance(choice)*-1)
     print(dijkstra.get_path(choice))
 
-#for the classic trolly problem
-inf_diagram2([[1,1,1,1,1],[1]])
-# inf_diagram1()
+def inf_diagram5vB():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem, but with a brother instead of 1 person
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1to5"
+    end2 = "utilB"
+    choice = "end result"
+    graph.add_edge(start, end1, -5)
+    graph.add_edge(start, end2, -10)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
 
-#other trolly problems here, note what each variation is with a comment
+
+def inf_diagram2v1():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem, but with 2 ppl
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1to2"
+    end2 = "util3"
+    choice = "end result"
+    graph.add_edge(start, end1, -2)
+    graph.add_edge(start, end2, -1)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
 
 
+def inf_diagram2vB():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem, but with 2 ppl and brother
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1to2"
+    end2 = "util3"
+    choice = "end result"
+    graph.add_edge(start, end1, -2)
+    graph.add_edge(start, end2, -10)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+def inf_diagram1v1():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem, but with 1 person
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2"
+    choice = "end result"
+    graph.add_edge(start, end1, -1)
+    graph.add_edge(start, end2, -1)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+def inf_diagram1vB():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem, but with 1 person and brother
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2"
+    choice = "end result"
+    graph.add_edge(start, end1, -1)
+    graph.add_edge(start, end2, -10)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+def inf_diagramBv1():
+    #method using a graph, this is definition 2 of intention in the paper
+    #classic trolley problem, but with brother and 1 person
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2"
+    choice = "end result"
+    graph.add_edge(start, end1, -10)
+    graph.add_edge(start, end2, -1)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+def inf_diagram1v2():
+    #method using a graph, this is definition 2 of intention in the paper
+    #1 person on main track, 2 ppl on side track
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2to3"
+    choice = "end result"
+    graph.add_edge(start, end1, -1)
+    graph.add_edge(start, end2, -2)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+def inf_diagramBv2():
+    #method using a graph, this is definition 2 of intention in the paper
+    #brother on main track, 2 people on side track
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2to3"
+    choice = "end result"
+    graph.add_edge(start, end1, -10)
+    graph.add_edge(start, end2, -2)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+
+def inf_diagram1v5():
+    #method using a graph, this is definition 2 of intention in the paper
+    #1 person on main track, 5 people on side track
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2to6"
+    choice = "end result"
+    graph.add_edge(start, end1, -1)
+    graph.add_edge(start, end2, -5)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+def inf_diagramBv5():
+    #method using a graph, this is definition 2 of intention in the paper
+    #brother on main track, 5 people on side track
+    graph = Graph()
+    start = "throw_A"
+    end1 = "util1"
+    end2 = "util2to6"
+    choice = "end result"
+    graph.add_edge(start, end2, -10)
+    graph.add_edge(start, end1, -5)
+    graph.add_edge(end2, choice, 0)
+    graph.add_edge(end1, choice, 0)
+    dijkstra = DijkstraSPF(graph, start)
+    print(dijkstra.get_distance(choice)*-1)
+    print(dijkstra.get_path(choice))
+
+
+#for the classic trolly problem (5 on main, 1 on side track)
+inf_diagram5v1()
 
 
 
